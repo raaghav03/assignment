3cdad333f09aebff7a80fa05cb362133d84862ca
raaghav03 <raghavnagpal2003@gmail.com>
2024-05-07 11:35:54 +0530

    added lucide icon lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Upload } from "lucide-react";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <button className="flex rounded-full  px-6 py-4 gap-2 m-4 border border-stone-200 text-gray-900">
+        <Upload />
+        <div>Upload your file</div>
+      </button>
+    </>
+  );
 }
