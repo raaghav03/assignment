6f15490dfdc8a540ec0d12045050ee1783587730
raaghav03 <raghavnagpal2003@gmail.com>
2024-05-07 12:15:59 +0530

    text file upload and content visible

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
 import { Upload } from "lucide-react";
 
 export default function App() {
+  const [file, setFile] = useState(null);
+  const [fileContent, setFileContent] = useState("");
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile && selectedFile.type === "text/plain") {
+      setFile(selectedFile);
+      readFileContent(selectedFile);
+    } else {
+      console.error("Please select a text file (*.txt)");
+    }
+  };
+  const readFileContent = (file) => {
+    const fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      setFileContent(e.target.result);
+    };
+    fileReader.readAsText(file);
+  };
   return (
     <>
-      <button className="flex rounded-full  px-6 py-4 gap-2 m-4 border border-stone-200 text-gray-900">
+      <button> Search Text</button>
+      <label className="flex rounded-full px-6 py-4 gap-2 m-4 border border-stone-200 text-gray-900 cursor-pointer">
         <Upload />
-        <div>Upload your file</div>
-      </button>
+        <p>{file ? file.name : "Upload your file"}</p>
+        <input
+          type="file"
+          accept=".txt "
+          onChange={handleFileChange}
+          className="hidden"
+        ></input>
+      </label>
+      {fileContent && (
+        <div className="m-4 p-4 border border-stone-200 rounded-md w-full">
+          <pre className="text-wrap">{fileContent}</pre>
+        </div>
+      )}
     </>
   );
 }
